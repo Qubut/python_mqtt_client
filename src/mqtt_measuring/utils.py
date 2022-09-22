792ea7c994eb90859d54ade68495c97107301275
Qubut <s-aahmed@haw-landshut.de>
2022-09-22 03:00:14 +0200

    Added Docstring to module utils.py and it's functions

--- src/mqtt_measuring/utils.py
+++ src/mqtt_measuring/utils.py
@@ -1,36 +1,115 @@
 from glob import glob
 import logging
 import os.path
-import asyncio
 from hashlib import md5
 from json import dumps, loads
 
+"""
+This is a module containing 
+the functions used by both publish.py and subscribe.py
+    
+"""
 
-def get_size(fpath):
+def get_size(fpath: str) -> int:
+    """returns the size of the given file
+
+    Args:
+        fpath (str): the full or relative path of the file
+
+    Returns:
+        int: the size of the file in KB 
+    """
     return int(os.path.getsize(fpath)/1024)
 
 
-def dump_json(msg): return dumps(msg)
-def load_json(msg): return loads(msg)
-def chunk_md5(chunk): return md5(chunk).hexdigest()
+def dump_json(msg: bytes) -> str:
+    """calls the function json.dumps to serialize an object
+    to a json formatted str
+
+    Args:
+        msg (bytes): the payload to be published by the MQTT client
+
+    Returns:
+        str: A json formatted string
+    """
+    return dumps(msg)
 
 
-def check_file(f):
+def load_json(msg: str) -> bytes:
+    """calls the function json.loads to deserialize a json formatted string to an object
+
+    Args:
+        msg (str): the payload recieved from the publisher
+
+    Returns:
+        object: the deserialized object json fromatted payload
+    """
+    return loads(msg)
+
+
+def chunk_md5(chunk: bytes) -> str:
+    """returns the md5 hash of a chunk of bytes
+
+    Args:
+        chunk (bytes): the chunk of data to be hashed
+
+    Returns:
+        str: the md5 hash of the chunk of data
+    """
+    return md5(chunk).hexdigest()
+
+
+def check_file(f: str) -> bool:
+    """checks if a file exits in the path
+
+    Args:
+        f (str): the full or relative path of the file
+
+    Returns:
+        bool: the result of checking the file's existance
+    """
     return os.path.isfile(f)
 
 
-def check_path(path): return os.path.exists(path)
+def check_path(path: str) -> bool:
+    """checks if the given path is valid
+
+    Args:
+        path (str): the path to a directory in the OS
+
+    Returns:
+        bool: the result for the checking the path
+    """
+    return os.path.exists(path)
 
 
-def mk_dir(dir): return os.makedirs(dir)
+def mk_dir(dir: str):
+    """makes a dir in the given path
+
+    Args:
+        dir (str): the full or relative path to the directory
+
+    Returns:
+        None
+    """
+    os.makedirs(dir)
 
 
-def exit(err):
+def exit(err: int):
+
     os._exit(err)
     os.kill(os.getpid)
 
 
-def generate_md5(fname):
+def generate_md5(fname) -> str:
+    """generates the md5 hash of a given file
+
+    Args:
+        fname (str): full or relative path to the file
+
+    Returns:
+        str: the md5 hash of the file
+    """
     hash_md5 = md5()
     with open(fname, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
@@ -38,7 +117,7 @@ def generate_md5(fname):
     return hash_md5.hexdigest()
 
 
-def check_temp_files(od, filename, timeid, filehash):
+def check_temp_files(od: str, filename: str, timeid: int, filehash: str):
     """ check temp file and rename to original
     """
     os.sync()
